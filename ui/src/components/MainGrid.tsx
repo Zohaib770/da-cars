8899442e680e843cea33e78ec731b58dc3db51d8
Zohaib Khalid <kzohaib77@gmail.com>
2024-12-04 16:18:35 +0100

    add list to main grid

--- ui/src/components/MainGrid.tsx
+++ ui/src/components/MainGrid.tsx
@@ -1,11 +1,16 @@
-// src/components/MainGrid.tsx
 import React from "react";
+import { useTranslation } from "react-i18next";
+import "./MainGrid.css";
 
 const MainGrid: React.FC = () => {
+  const{t} = useTranslation();
+
   return (
     <div className="main-grid">
-      
-
+      <div className="grid-item">{t("KFZAufbereitung")}</div>
+      <div className="grid-item">{t("Scheibentönung")}</div>
+      <div className="grid-item">{t("Autoglasservice")}</div>
+      <div className="grid-item">{t("klimaservice")}</div>
     </div>
   );
 };
--- ui/src/i18n/i18.tsx
+++ ui/src/i18n/i18.tsx
@@ -1,18 +1,32 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import {initReactI18next, Translation} from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
     lng:"de",
     resources:{
         de:{
             translation:{
-                dienstleistungen: "dienstleistungen"
+                Dienstleistungen: "Dienstleistungen",
+                Fahrzeuge: "Fahrzeuge",
+                Kontakt: "Kontakt",
+                einloggen: "Einloggen",
+                KFZAufbereitung: "KFZ-Aufbereitung",
+                Scheibentönung: "Scheibentönung",
+                Autoglasservice: "Autoglasservice",
+                klimaservice: "Klimaservice"
             }
         },
         fr:{
             translation:{
-                
+                dienstleistungen: "Services",
+                Fahrzeuge: "Véhicule",
+                Kontakt: "Contact",
+                einloggen: "s'identifier",
+                KFZAufbereitung: "Préparation du véhicule",
+                Scheibentönung: "Teinte des verres",
+                Autoglasservice: "Service de vitres de voiture",
+                klimaservice: "Service de climatisation"
             }
         }
     }
