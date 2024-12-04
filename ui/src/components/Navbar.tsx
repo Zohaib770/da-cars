a6c95a9d130622a6c6bcc61588ca59f605e28333
Zohaib Khalid <kzohaib77@gmail.com>
2024-12-04 16:19:27 +0100

    change font size/add transaltion

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -1,9 +1,18 @@
-// src/components/Navbar.tsx
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import daCars from '../assets/da-cars-logo.png';
+import { useTranslation } from "react-i18next";
 
 const Navbar: React.FC = () => {
+
+  const{t, i18n} = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const changeLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const lang = event.target.value;
+    i18n.changeLanguage(lang);
+  };
+
   return (
     <nav>
       <div className="navbar-container">
@@ -12,11 +21,17 @@ const Navbar: React.FC = () => {
         </div>
         <div className="navbar-right">
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#home">{t("Dienstleistungen")}</a></li>
+            <li><a href="#about">{t("Fahrzeuge")}</a></li>
+            <li><a href="#services">{t("Kontakt")}</a></li>
+            <li><a href="#contact">{t("einloggen")}</a></li>
           </ul>
+          {/* <div className="language-selector"> */}
+            <select onChange={changeLanguage} defaultValue={i18n.language}>
+              <option value="de">DE</option>
+              <option value="fr">FR</option>
+            </select>
+          {/* </div> */}
         </div>
       </div>
     </nav>
