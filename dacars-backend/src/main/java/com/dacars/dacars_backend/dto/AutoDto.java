acd7740b70cbccb8de9b6d6fe743b2b201f2e7d9
Zohaib Khalid <kzohaib77@gmail.com>
2024-12-31 22:36:50 +0100

    rmv unused import

--- dacars-backend/src/main/java/com/dacars/dacars_backend/dto/AutoDto.java
+++ dacars-backend/src/main/java/com/dacars/dacars_backend/dto/AutoDto.java
@@ -2,8 +2,6 @@ package com.dacars.dacars_backend.dto;
 
 import java.util.List;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
