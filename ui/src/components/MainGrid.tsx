13f8ee8a5c2211c1fce503be3451be34e3ba38a3
Zohaib Khalid <kzohaib77@gmail.com>
2025-01-07 16:18:15 +0100

    update css

--- ui/src/components/MainGrid.tsx
+++ ui/src/components/MainGrid.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import "./maingridpages/MainGridPages.css";
 import { Link } from 'react-router-dom';
 
+
 const MainGrid: React.FC = () => {
   const{t} = useTranslation();
 
