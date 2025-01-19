e2b08b0d844d0d276cd1bf7164da62c209815eae
Zohaib Khalid <kzohaib77@gmail.com>
2025-01-19 07:49:09 +0100

    rmv unused import

--- dacars-backend/src/main/java/com/dacars/dacars_backend/service/EmailService.java
+++ dacars-backend/src/main/java/com/dacars/dacars_backend/service/EmailService.java
@@ -1,7 +1,5 @@
 package com.dacars.dacars_backend.service;
 
-import java.security.SecureRandom;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
