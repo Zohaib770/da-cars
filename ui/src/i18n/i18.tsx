7679d352fb7a9643b3d35e6c1a47a0fb23d3b766
Zohaib Khalid <kzohaib77@gmail.com>
2025-03-08 11:45:28 +0100

    update language

--- ui/src/i18n/i18.tsx
+++ ui/src/i18n/i18.tsx
@@ -7,17 +7,28 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
     resources:{
         de:{
             translation:{
-                //wilkommen Text
+                //MainGrid Text
                 dacar: "Da Cars",
-                dacar_text: "Auto An- & Verkauf, Professionelle KFZ-Pflege, FOLIATE" +
+
+                maingrid_herocontent_title: "Professionelle Fahrzeugpflege auf höchstem Niveau",
+                maingrid_herocontent_text: "Wir bieten erstklassige Aufbereitung, Scheibentönung und Fahrzeugpflege für anspruchsvolle Kunden.",
+                maingrid_unsere_dienstleistungen: "Unsere Dienstleistungen",
+                maingrid_professionelle_loesungen_fuer_ihr_fahrzeug: "Professionelle Lösungen für Ihr Fahrzeug",
+
+                maingrid_ueber_dacar: "Über DA-Cars",
+                maingrid_dacar_text: "Auto An- & Verkauf, Professionelle KFZ-Pflege, FOLIATE" +
                             "Scheibentönung, Alufelgen, Reifen, Autoglasservice, Klimaservice", 
-                dacar_wilk_text: "Willkommen bei DA-Cars, Ihrer professionellen Lösung für die Aufbereitung" +
-                                "und Pflege Ihres Fahrzeug. Wir sind Ihr vertrauenswürdiger Partner, wenn es" +
-                                "darum geht, Ihrem Auto neuen Glanz zu verleihen und es in bestmöglichem Zustand" +
-                                "zu erhalten. Wir bieten qualitativ hochwertigen Service, auf den Sie sich verlassen" +
+                maingrid_dacar_wilk_text: "Willkommen bei DA-Cars, Ihrer professionellen Lösung für die Aufbereitung " +
+                                "und Pflege Ihres Fahrzeug. Wir sind Ihr vertrauenswürdiger Partner, wenn es " +
+                                "darum geht, Ihrem Auto neuen Glanz zu verleihen und es in bestmöglichem Zustand " +
+                                "zu erhalten. Wir bieten qualitativ hochwertigen Service, auf den Sie sich verlassen " +
                                 "können! Egal ob Sie eine Grundreinigung, Scheibentönung oder Glasservice benötigen." +
                                 "Mit hochwertigen Produkten und modernster Technik verleihen wir Ihrem Fahrzeug ein" +
                                 "makelloses Aussehen.",
+
+                maingrid_jahre_erfahrung: "Jahre Erfahrung",
+                maingrid_zufriedene_kunden: "Zufriedene Kunden",
+                maingrid_qualitaetsgarantie: "Qualitätsgarantie",
 
                 // Navbar
                 dienstleistungen: "Dienstleistungen",
@@ -36,6 +47,7 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
                 kfzaufbereitungverwalten: "Kfz-Aufbereitung verwaltung",
                 preis_liste_kfzaufbereitung:"Preisliste KFZ-Aufbereitung",
                 keine_preis_liste_kfzaufbereitung: "Preise noch nicht vorhanden",
+                autopflege: "Autopflege",
                 dienste: "Dienste",
                 beschreibung: "Beschreibung",
                 pkw_preis: "PKW-Preis",
@@ -43,14 +55,22 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
                 kleinbus_preis: "Kleinbus Preis",
                 aktion: "Aktion",
                 aktualisieren: "Aktualisieren",
-                
+                kfzaufbereitungverwalten_success: "erfolgreich aktualisiert !",
+                kfzaufbereitungverwalten_error: "Es ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut",
+
+                komplettreinigung: "Komplettreinigung",
+                komplettreinigung_text: "enthält alle in diesem Abschnitt aufgeführten Leistungen",
+
                 aussenreinigung: "Außenreinigung",
-                aussenreinigung_text: "Eine gründliche Außenreinigung schützt den Lack, lässt Ihr Fahrzeug in" +
-                                    "neuem Glanz erstrahlen und bewahrt langfristig seinen Wert.",
-                
-                aussenwaesche: "Außenwäsche",
-                aussenwaesche_shampoo: "mit Wax Shampoo",
-                
+                aussenreinigung_nasswaesche: "Nasswäsche von Hand",
+                aussenreinigung_felgen: "Felgen - Spezialreinigung",
+                aussenreinigung_motor: "Motor - Spezialreinigung",
+
+                Innenreinigung_standard: "Innenreinigung Standard",
+                Innenreinigung_standard_text: "Innenraum aussaugen, Scheiben- und Cockpitpflege",
+                Innenreinigung_intensive: "Innenreinigung Intensive",
+                Innenreinigung_intensive_text: " ",
+
                 lackpflege: "Lackpflege",
                 lackpflege_politur: "-	Politur " +
                                         "(Entfernt kleine Kratzer, reinigt den Lack und erzeugt ein" +
@@ -60,13 +80,9 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
                 lackpflege_24: "Nano Lackbeschichtung (Bis zu 24 Monaten)",
                 lackpflege_36: "Keramik Lackversiegelung (Bis zu 36 Monaten)",
 
-                Innenreinigung: "Innenreinigung",
-                Innenreinigung_standard: "Standard Reinigung (Innenraum aussaugen," +
-                                        "Scheiben- und Cockpitpflege)",
-                Innenreinigung_intensive: "Intensiv Reinigung (Standard Reinigung + Kofferraum aussaugen," +
-                                            "Kunststoffpflege,Polsterreinigung, Lederpflege, Dachhimmel-Reinigung," +
-                                            "Türkantenreinigung)",
-
+                aussenwaesche: "Außenwäsche",
+                aussenwaesche_shampoo: "mit Wax Shampoo",
+                
                 stoff_textil: "Stoff/Textil Imprägnierung (Cabriodach)",
 
                 motorwaesche: "Motorwäsche",
@@ -226,18 +242,29 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
         },
         fr:{
             translation:{
-                //wilkommen Text
+                //MainGrid Text
                 dacar: "Da Cars",
-                dacar_text: "Achat et vente de voitures, entretien professionnel, teintage" +
-                            "des vitres FOLIATEC, jantes en alliage, pneus, service de vitrage" +
-                            "automobile, service de climatisation.", 
-                dacar_wilk_text: "Bienvenue chez Da Cars, votre solution professionnelle pour l’entretien" +
+
+                maingrid_herocontent_title: "Entretien professionnel des véhicules au plus haut niveau",
+                maingrid_herocontent_text: "Nous offrons une préparation de premier ordre, la teinture des vitres et l’entretien des véhicules pour les clients exigeants.",
+                maingrid_unsere_dienstleistungen: "Nos services",
+                maingrid_professionelle_loesungen_fuer_ihr_fahrzeug: "Des solutions professionnelles pour votre véhicule",
+
+                maingrid_ueber_dacar: "Über DA-Cars",
+                maingrid_dacar_text: "Achat et vente de voitures, entretien professionnel, teintage" +
+                                    "des vitres FOLIATEC, jantes en alliage, pneus, service de vitrage" +
+                                    "automobile, service de climatisation.",
+                maingrid_dacar_wilk_text: "Bienvenue chez Da Cars, votre solution professionnelle pour l’entretien" +
                                 "et la préparation de votre véhicule. Nous sommes votre partenaire de confiance" +
                                 "pour redonner de l’éclat à votre voiture et la maintenir en parfait état. Nous" +
                                 "proposons un service de haute qualité sur lequel vous pouvez compter ! Que vous" +
                                 "ayez besoin d’un nettoyage, d’un teintage de vitres ou d’un service de vitrage," +
                                 "nous utilisons des produits haut de gamme et des technologies modernes pour offrir" +
                                 "à votre véhicule une apparence impeccable.",
+
+                maingrid_jahre_erfahrung: "Années d’expérience",
+                maingrid_zufriedene_kunden: "Des clients satisfaits",
+                maingrid_qualitaetsgarantie: "Qualité",
 
                 // Navbar
                 dienstleistungen: "Services",
@@ -256,6 +283,7 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
                 kfzaufbereitungverwalten: "Administration de la préparation du véhicule",
                 preis_liste_kfzaufbereitung:"Liste de prix pour préparation automobile",
                 keine_preis_liste_kfzaufbereitung: "Prix pas encore disponibles",
+                autopflege: "Entretien de voiture",
                 dienste: "Services",
                 beschreibung: "Description",
                 pkw_preis: "Prix pour voiture particulière (PKW)",
@@ -263,11 +291,25 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
                 kleinbus_preis: "Prix pour minibus",
                 aktion: "Action",
                 aktualisieren: "mettre à jour",
+                kfzaufbereitungverwalten_success: "Mise à jour réussie !",
+                kfzaufbereitungverwalten_error: "Une erreur s’est produite. Veuillez réessayer",
+
+                komplettreinigung: "Nettoyage complet",
+                komplettreinigung_text: "Comprend tous les services énumérés dans cette section",
 
                 aussenreinigung: "Nettoyage extérieur",
-                aussenreinigung_text: "Un nettoyage extérieur complet protège la peinture, redonne" +
-                                        "à votre véhicule son éclat et préserve sa valeur à long terme.",
-                
+                aussenreinigung_nasswaesche: "Lavage humide à la main",
+                aussenreinigung_felgen: "Jantes - Nettoyage spécial",
+                aussenreinigung_motor: "Moteur - Nettoyage spécial",
+
+                Innenreinigung_standard: "Nettoyage intérieur Standard",
+                Innenreinigung_standard_text: "Aspiration de l’intérieur, nettoyage" +
+                                            "des vitres et du tableau de bord",
+                Innenreinigung_intensive: "Nettoyage intérieur intensif",
+                Innenreinigung_intensive_text: "Nettoyage standard + aspiration du" +
+                                            "coffre, soin des plastiques, nettoyage des tissus, soin" +
+                                            "du cuir, nettoyage du ciel de toit et des bords de porte",
+
                 aussenwaesche: "Lavage extérieur",
                 aussenwaesche_shampoo: "Avec shampooing à la cire",
                 
@@ -279,13 +321,6 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
                 lackpflege_versiegelung: "Protection de la peinture (Scellement longue durée)",
                 lackpflege_24: "Revêtement nano (-	Jusqu’à 24 mois)",
                 lackpflege_36: "Scellement céramique (Jusqu’à 36 mois)",
-
-                Innenreinigung: "Nettoyage intérieur",
-                Innenreinigung_standard: "Nettoyage standard (Aspiration de l’intérieur, nettoyage" +
-                                            "des vitres et du tableau de bord)",
-                Innenreinigung_intensive: "Nettoyage intensif (Nettoyage standard + aspiration du" +
-                                            "coffre, soin des plastiques, nettoyage des tissus, soin" +
-                                            "du cuir, nettoyage du ciel de toit et des bords de porte)",
 
                 stoff_textil: "Imprégnation des tissus/textiles (toit de cabriolet)",
 
@@ -341,6 +376,71 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
                 absenden: "Dépêche",
                 kontakt_success: "Message envoyé avec succès !",
                 kontakt_error: "Une erreur s’est produite. Veuillez réessayer",
+
+                //Impressum
+                impressum: "Impressum",
+                geschaeftsfuehrer: "Vertretungsberechtigte Geschäftsführer: Djilali Ameur",
+                umsatzsteuer_id: "Umsatzsteuer-Identifikationsnummer gemäß § 27 a",
+                umsatzsteuer_gesetzte: "Umsatzsteuergesetz: DE231110586",
+                impr_erst: "Plattform der Europäischen Kommission zur Online-Streitbeilegung (OS) für Verbraucher:",
+                impr_zweit: ". Wir sind nicht bereit und nicht verpflichtet an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.",
+
+                //Haftungsausschluss
+                haftungsausschluss: "Haftungsausschluss",
+                inhalt: "1. Inhalt des Onlineangebotes",
+                inhalt_text: "Der Autor übernimmt keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder" +
+                            "Qualität der bereitgestellten Informationen. Haftungsansprüche gegen den Autor, welche sich" +
+                            "auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung" +
+                            "der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger" +
+                            "Informationen verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des" +
+                            "Autors kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt. Alle" +
+                            "Angebote sind freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile" +
+                            "der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu" +
+                            "löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.",
+                verweis: "2. Verweise und Links",
+                verweis_text: "Bei direkten oder indirekten Verweisen auf fremde Webseiten (“Hyperlinks”), die außerhalb" +
+                            "des Verantwortungsbereiches des Autors liegen, würde eine Haftungsverpflichtung ausschließlich" + 
+                            "in dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch" +
+                            "möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern. Der Autor" +
+                            "erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte auf den" +
+                            "zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die Inhalte" +
+                            "oder die Urheberschaft der verlinkten/verknüpften Seiten hat der Autor keinerlei Einfluss. Deshalb" +
+                            "distanziert er sich hiermit ausdrücklich von allen Inhalten aller verlinkten /verknüpften Seiten, die" +
+                            "nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen" +
+                            "Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten" +
+                            "Gästebüchern, Diskussionsforen, Linkverzeichnissen, Mailinglisten und in allen anderen Formen von" +
+                            "Datenbanken, auf deren Inhalt externe Schreibzugriffe möglich sind. Für illegale, fehlerhafte oder" +
+                            "unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart" +
+                            "dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen" +
+                            "wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.",
+                urheber: "3. Urheber- und Kennzeichenrecht",
+                urheber_text: "Der Autor ist bestrebt, in allen Publikationen die Urheberrechte der verwendeten Bilder," +
+                    	    "Grafiken, Tondokumente, Videosequenzen und Texte zu beachten, von ihm selbst erstellte Bilder," +
+                            "Grafiken, Tondokumente, Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken," +
+                            "Tondokumente, Videosequenzen und Texte zurückzugreifen. Alle innerhalb des Internetangebotes" +
+                            "genannten und ggf. durch Dritte geschützten Marken- und Warenzeichen unterliegen uneingeschränkt" + 
+                            "den Bestimmungen des jeweils gültigen Kennzeichenrechts und den Besitzrechten der jeweiligen" +
+                            "eingetragenen Eigentümer. Allein aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen, dass" +
+                            "Markenzeichen nicht durch Rechte Dritter geschützt sind! Das Copyright für veröffentlichte, vom Autor" +
+                            "selbst erstellte Objekte bleibt allein beim Autor der Seiten. Eine Vervielfältigung oder Verwendung" +
+                            "solcher Grafiken, Tondokumente, Videosequenzen und Texte in anderen elektronischen oder gedruckten" +
+                            "Publikationen ist ohne ausdrückliche Zustimmung des Autors nicht gestattet.",
+                datenschutz: "4. Datenschutz",
+                datenschutz_text: "Sofern innerhalb des Internetangebotes die Möglichkeit zur Eingabe persönlicher" +
+                                "oder geschäftlicher Daten (Emailadressen, Namen, Anschriften) besteht, so erfolgt" +
+                                "die Preisgabe dieser Daten seitens des Nutzers auf ausdrücklich freiwilliger Basis." +
+                                "Die Inanspruchnahme und Bezahlung aller angebotenen Dienste ist – soweit technisch" +
+                                "möglich und zumutbar – auch ohne Angabe solcher Daten bzw. unter Angabe anonymisierter" +
+                                "Daten oder eines Pseudonyms gestattet. Die Nutzung der im Rahmen des Impressums oder" +
+                                "vergleichbarer Angaben veröffentlichten Kontaktdaten wie Postanschriften, Telefon- und" +
+                                "Faxnummern sowie Emailadressen durch Dritte zur Übersendung von nicht ausdrücklich" +
+                                "angeforderten Informationen ist nicht gestattet. Rechtliche Schritte gegen die Versender" +
+                                "von sogenannten Spam-Mails bei Verstössen gegen dieses Verbot sind ausdrücklich vorbehalten.",
+                rechte: "5. Rechtswirksamkeit dieses Haftungsausschlusses",
+                rechte_text : "Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von" +
+                            "dem aus auf diese Seite verwiesen wurde. Sofern Teile oder einzelne Formulierungen" +
+                            "dieses Textes der geltenden Rechtslage nicht, nicht mehr oder nicht vollständig entsprechen" +
+                            "sollten, bleiben die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit davon unberührt.",
 
                 //Einloggen
                 anmelden: "annoncer",
