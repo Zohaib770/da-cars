441850a930357d643c83858d7d717ad0797aec82
Zohaib Khalid <kzohaib77@gmail.com>
2025-03-10 13:16:09 +0100

    update: inserts/hauptbild

--- dacars-backend/src/main/resources/db-script/dml.sql
+++ dacars-backend/src/main/resources/db-script/dml.sql
@@ -4,20 +4,21 @@ VALUES ('admin@gmail.com', '$2a$10$wWy61t9rwpY6XzXjHtoVe.byVTM79QdEG990m9c7p4AcE
 
 INSERT INTO kfzaufbereitungpreise (id, autopflege, kleinbus_preis, pkw_preis, van_suv_preis) 
 VALUES 
-('1', 'komplettreinigung_text', '99.99', '79.99', '89.99'),
-('2', 'aussenreinigung_nasswaesche', '19.99', '14.99', '17.99'),
-('3', 'aussenreinigung_felgen', '29.99', '19.99', '24.99'),
-('4', 'aussenreinigung_motor', '39.99', '29.99', '34.99'),
-('5', 'aussenwaesche_shampoo', '39.99', '19.99', '29.99'),
-('6', 'Innenreinigung_standard_text', '39.99', '19.99', '29.99'),
-('7', 'Innenreinigung_intensive_text', '59.99', '39.99', '49.99'),
-('8', 'lackpflege_politur', '69.99', '49.99', '59.99'),
-('9', 'lackpflege_schleife', '69.99', '49.99', '59.99'),
-('10', 'lackpflege_versiegelung', '69.99', '49.99', '59.99'),
-('11', 'lackpflege_24', '69.99', '49.99', '59.99'),
-('12', 'lackpflege_36', '79.99', '59.99', '69.99'),
-('13', 'stoff_textil', '44.99', '24.99', '34.99'),
-('14', 'motorwaesche_text', '49.99', '29.99', '39.99'),
-('15', 'glasversiegelung_text', '69.99', '49.99', '59.99');
-
-
+('1', 'aussenreinigung_nasswaesche', '19.99', '14.99', '17.99'),
+('2', 'aussenreinigung_felgen', '29.99', '19.99', '24.99'),
+('3', 'aussenreinigung_motor', '39.99', '29.99', '34.99'),
+('4', 'aussenwaesche_shampoo', '39.99', '19.99', '29.99'),
+('5', 'Innenreinigung_standard_text', '39.99', '19.99', '29.99'),
+('6', 'Innenreinigung_intensive_text', '59.99', '39.99', '49.99'),
+('7', 'lackpflege_politur', '69.99', '49.99', '59.99'),
+('8', 'lackpflege_schleife', '69.99', '49.99', '59.99'),
+('9', 'lackpflege_versiegelung', '69.99', '49.99', '59.99'),
+('10', 'lackpflege_24', '69.99', '49.99', '59.99'),
+('11', 'lackpflege_36', '79.99', '59.99', '69.99'),
+('12', 'stoff_textil', '44.99', '24.99', '34.99'),
+('13', 'motorwaesche_text', '49.99', '29.99', '39.99'),
+('14', 'glasversiegelung_text', '69.99', '49.99', '59.99');
+('15', 'ozonbehandlung_text', '69.99', '49.99', '59.99');
+('16', 'polsterreparatur_text', '69.99', '49.99', '59.99');
+('17', 'glasversiegelung_text', '69.99', '49.99', '59.99');
+('18', 'glasversiegelung_text', '69.99', '49.99', '59.99');
